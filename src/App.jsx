fb0594db729be29d8f0eb9cfc5b543bbecb093cb
Param Sahai <sahai.param.2002@gmail.com>
2022-07-14 22:09:00 +0530

    added sass, got to know about styles(scss)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Board from './Board';
+import Board from './components/Board';
 const App = () => {
   return (
-    <>
+    <div className="app">
       <h1>Tic Tac Toe</h1>
       <Board />
-    </>
+    </div>
   );
 };
 export default App;
--- src/Square.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Square = ({ value }) => {
-  return <button type="button">{value}</button>;
-};
-
-export default Square;
