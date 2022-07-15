11b89063153e3a3280fb7c13417cdc226d92bf31
Param Sahai <sahai.param.2002@gmail.com>
2022-07-15 12:27:10 +0530

    Added winner logic and messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './components/Board';
+import { calculateWinner } from './Winner';
 const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isNext, setIsNext] = useState(false);
+
+  const winner = calculateWinner(board);
+
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next is ${isNext ? 'X' : 'O'}`;
+
+  const handleSquareClick = position => {
+    if (board[position] || winner) {
+      return;
+    }
+
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isNext ? 'X' : 'O';
+        }
+        return square;
+      });
+    });
+
+    setIsNext(prev => !prev);
+  };
+
   return (
     <div className="app">
       <h1>Tic Tac Toe</h1>
-      <Board />
+      <h3>{message}</h3>
+      <Board board={board} handleSquareClick={handleSquareClick} />
     </div>
   );
 };
