62c0da3f76970fdabae76bc500f78f28070c8780
Param Sahai <sahai.param.2002@gmail.com>
2022-07-18 14:06:16 +0530

    Highlighted Winner and Added Start New Button

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import History from './components/History';
 import StatusMessage from './components/StatusMessage';
 import { calculateWinner } from './Winner';
 
+const NewGame = [{ board: Array(9).fill(null), isXNext: true }];
+
 const App = () => {
-  const [history, setHistory] = useState([
-    { board: Array(9).fill(null), isXNext: true },
-  ]);
+  const [history, setHistory] = useState(NewGame);
   const [currentMove, setCurrentMove] = useState(0);
 
   const current = history[currentMove];
 
   // console.log('history', history);
 
-  const winner = calculateWinner(current.board);
+  const { winner, winningSquares } = calculateWinner(current.board);
 
   const handleSquareClick = position => {
     if (current.board[position] || winner) {
@@ -42,11 +42,22 @@ const App = () => {
     setCurrentMove(move);
   };
 
+  const onNewGame = () => {
+    setHistory(NewGame);
+    setCurrentMove(0);
+  };
   return (
     <div className="app">
       <h1>Tic Tac Toe</h1>
       <StatusMessage winner={winner} current={current} />
-      <Board board={current.board} handleSquareClick={handleSquareClick} />
+      <Board
+        board={current.board}
+        handleSquareClick={handleSquareClick}
+        winningSquares={winningSquares}
+      />
+      <button type="button" onClick={onNewGame}>
+        Start a New Game
+      </button>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
     </div>
   );
